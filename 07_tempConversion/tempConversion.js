b7ff43efe028ee0ab107ce71fc12581629e2de28
prasannaprabhud <prabhuverse1@gmail.com>
2025-03-12 19:59:40 +0530

    TOP Foundation course - Test Driven Development is Finished

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  // °C = (°F − 32) × 5/9.
+  const fahrenheit = temp;
+  const convertionFormula = (fahrenheit - 32) * 5/9;
+  return Math.round(convertionFormula) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  // °F = °C × 9/5 + 32.
+  const celsius = temp;
+  const convertionFormula = celsius * 9/5 + 32;
+  return Math.round(convertionFormula) / 10;
 };
 
 // Do not edit below this line
